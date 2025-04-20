f6a8c95fb494461f45f398a7163f26e89ec9181f
Jean-Marc Prieur <jmprieur@microsoft.com>
2025-04-19 18:03:04 -0700

    Package of the generator. See https://github.com/dotnet/roslyn/blob/main/docs/features/source-generators.cookbook.md#package-a-generator-as-a-nuget-package

--- ManualTest/Program.cs
+++ ManualTest/Program.cs
@@ -1,24 +1,28 @@
-﻿using CopyFromGenerator;
+﻿namespace ManualTest;
 
-namespace ManualTest;
+[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+public sealed class GenerateCopyFromMethodAttribute : Attribute
+{
+}
 
 [GenerateCopyFromMethod]
 public partial class Person
 {
     public string Name { get; set; } = "";
     public int Age { get; set; }
+    public string Address { get; set; } = "";
 }
 
 public class Program
 {
     public static void Main()
     {
-        var person1 = new Person { Name = "John", Age = 30 };
+        var person1 = new Person { Name = "John", Age = 30, Address = "USA" };
         var person2 = new Person();
         
         person2.CopyFrom(person1);
         
-        Console.WriteLine($"Person 2 - Name: {person2.Name}, Age: {person2.Age}");
+        Console.WriteLine($"Person 2 - Name: {person2.Name}, Age: {person2.Age}, Age: {person2.Address}");
         // Should output: Person 2 - Name: John, Age: 30
     }
 }
